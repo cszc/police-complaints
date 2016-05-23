05e03fa7deeaae9132f91acad5d6e64b4a63a9d1
ladyson <dyson.lauren@gmail.com>
2016-05-23 18:00:13 -0500

    trying it without psycopg2

--- features/count_rodents.sql
+++ features/count_rodents.sql
@@ -3,10 +3,10 @@ UPDATE "311rodent" SET geom = ST_SetSRID(ST_MakePoint(lng,lat),4326);
 CREATE INDEX "idx_rodent_geom" ON "311rodent" USING GIST(geom);
 ALTER TABLE "311bytract" ADD rodent_count int;
 update "311bytract" as a
-inner join ( 
+set a.rodent_count = b.cnt
+from ( 
     select count(*) as cnt from "311rodent" as complaints 
     JOIN "tracts2010" as t 
     on ST_Contains(t.geom, complaints.geom) 
     group by t.tractce10) as b
-    on a.tractce10 = b.tractce10
-set a.rodent_count = b.cnt;
+where a.tractce10 = b.tractce10;
