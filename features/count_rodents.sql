c747dbf51e300937efacbaf680b5e3b63e69fcc7
ladyson <dyson.lauren@gmail.com>
2016-05-23 17:41:52 -0500

    trying it without psycopg2

--- features/count_rodents.sql
+++ features/count_rodents.sql
@@ -1,7 +1,7 @@
 ALTER TABLE "311rodent" ADD COLUMN geom geometry(POINT,4326);
 UPDATE "311rodent" SET geom = ST_SetSRID(ST_MakePoint(lng,lat),4326);
 CREATE INDEX "idx_rodent_geom" ON "311rodent" USING GIST(geom);
-ALTER "311bytract" ADD rodent_count int;
+ALTER TABLE "311bytract" ADD rodent_count int;
 update "311bytract" 
 set rodent_count = b.cnt 
 from "311bytract" as main join ( 
