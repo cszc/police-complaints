069c59435dc404e0686cfcc9542173f234aa47e0
ladyson <dyson.lauren@gmail.com>
2016-05-23 17:19:22 -0500

    what is going on with quotes

--- features/count_311.py
+++ features/count_311.py
@@ -4,7 +4,7 @@ from psycopg2.extensions import QuotedString
 
 
 MAIN_TABLE = "311bytract"
-TABLES_TO_COUNT = ['311alleylights', "311bytract", "311garbage", "311graffiti", "311potholes", "311rodent", "311sanitation", "311streetlightsall", "311streetlightsone", "311trees", "311vap","311vehicles"]
+TABLES_TO_COUNT = ["311alleylights", "311bytract", "311garbage", "311graffiti", "311potholes", "311rodent", "311sanitation", "311streetlightsall", "311streetlightsone", "311trees", "311vap","311vehicles"]
 
 class client:
     def __init__(self):
