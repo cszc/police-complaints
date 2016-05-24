e2a6144c5ff2564a34dc7d78f9f66e311e95e4e2
ladyson <dyson.lauren@gmail.com>
2016-05-24 16:51:24 -0500

    fixed typo

--- features/count_311.py
+++ features/count_311.py
@@ -3,7 +3,7 @@ from psycopg2.extensions import AsIs
 from psycopg2.extensions import QuotedString
 
 
-MAIN_TABLE = "\"311bytract\""
+MAIN_TABLE = "311bytract"
 # TABLES_TO_COUNT = ['"311alleylights"', "311bytract", "311garbage", "311graffiti", "311potholes", "311rodent", "311sanitation", "311streetlightsall", "311streetlightsone", "311trees", "311vap","311vehicles"]
 
 TABLES_TO_COUNT = ["311alleylights"]
@@ -38,26 +38,27 @@ class client:
 
     def count_311_calls(self, table311):
         name = "\""+str(table311)+"\""
+        main_name = "\""+MAIN_TABLE+"\""
         print("Starting {}".format(table311))
         cur = self.dbconn.cursor()
         # add a geometry column to an existing 311 table table
         cur.execute('''
             ALTER TABLE %s ADD COLUMN geom geometry(POINT,4326);
             ''', [AsIs(name)])
-        
+        print("success #1")
         # make a geopoint column from existing text lat & long columns
         # note that for some reason lat/lng are reverse from what you'd expect
         cur.execute("UPDATE %s SET geom = ST_SetSRID(ST_MakePoint(lng,lat),4326);", [AsIs(name)])
-
+        print("success #2")
         index_name = "idx_" + table311 + "_geom"
         # make an index
         cur.execute("CREATE INDEX %s ON %s USING GIST(geom);", (AsIs(index_name), AsIs(name)))
-         
+        print("success #3")
         # count how many 311 calls there were per census tract and add that to the main table
 
         col_name = table311 + "_count"
         cur.execute("alter %s add %s int;", (AsIs(MAIN_TABLE), AsIs(col_name)))
-
+        print("success #4")
         cur.execute("""
         update %s 
         set %s = b.cnt 
