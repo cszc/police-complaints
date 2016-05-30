e58104fa39d39b415c1b028885bd96eca566f7ee
ladyson <dyson.lauren@gmail.com>
2016-05-30 18:09:44 -0500

    updte

--- features/count_311.py
+++ features/count_311.py
@@ -322,9 +322,7 @@ if __name__ == "__main__":
 
     # count other complaints
     resultscomplaints = "time_distance_complaints"
-    # dbClient.make_new_feature_table(ALLEGATIONS_TABLE, resultscomplaints)
-    # print("Created {}".format(resultscomplaints))
-    # print("Starting aggregate {}".format(ALLEGATIONS_TABLE))
+    dbClient.make_new_feature_table(ALLEGATIONS_TABLE, resultscomplaints)
 
     dbClient.count_other_complaints(ALLEGATIONS_TABLE, resultscomplaints)
 
