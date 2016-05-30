ede02b0ca0226bdd49c416f3834387420a234a69
LAUREN <dyson.lauren@gmail.com>
2016-05-30 18:45:46 +0000

    working on query

--- query.py
+++ query.py
@@ -1,14 +1,14 @@
 import pandas as pd
-import pyscopg2 as pg2
+import psycopg2
 
 def go():
     conn = psycopg2.connect("dbname = police user = lauren password = llc")
 
 
-    invest_sel = "(SELECT * FROM investigator_beat_dum1 JOIN investigator_beat_dum2) as dummies \
+    invest_sel = "(SELECT * FROM investigator_beat_dum1 NATURAL JOIN investigator_beat_dum2) as dummies \
                 ON ((alleg.crid, alleg.officer_id) = (dummies.crid, dummies.officer_id))"
 
-    age_sel = "(SELECT crid, officer_id, officers_age from ages) as age \
+    age_sel = "(SELECT crid, officer_id, officers_age FROM ages) as age \
                 ON ((alleg.crid, alleg.officer_id) = (age.crid, age.officer_id))"
 
     data311_sel = "(SELECT * FROM time_distance_311) as d311 \
@@ -19,8 +19,8 @@ def go():
                 FROM acs) as acs \
                 ON (alleg.tractce10 = acs.tract_1)"
 
-    query = "SELECT * FROM (SELECT crid, officer_id, tractce10, FROM allegations_clean) as alleg JOIN \
-            {} JOIN {} JOIN {} JOIN {};".format(invest_sel, age_sel, data311_sel, acs_sel)"
+    query = "SELECT * FROM (SELECT crid, officer_id, tractce10 FROM allegations) as alleg JOIN \
+            {} JOIN {} JOIN {} JOIN {};".format(invest_sel, age_sel, data311_sel, acs_sel)
 
     df = pd.read_sql(query, conn, index_col = ['crid', 'officer_id'])
 
