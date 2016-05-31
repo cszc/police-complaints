1a178eb0e70591a4439ee4b9f3760bd2ba963131
lmcindewar <lmcindewar@gmail.com>
2016-05-30 20:06:43 -0500

    fixed query

--- query.py
+++ query.py
@@ -2,9 +2,14 @@ import pandas as pd
 import psycopg2
 
 def go():
+    '''Generate dataframe with features from database'''
+
     conn = psycopg2.connect("dbname = police user = lauren password = llc")
 
-    alleg = "SELECT crid, officer_id, tractce10 FROM allegations;"
+    #Queries for features
+    alleg = "SELECT crid, officer_id, tractce10, pct017, pct1824, pct2534, pct3544, pct4554, pct5564, pct6500, \
+                ptnla, ptnlb, ptnlwh, ptnloth, ptl, ptlths, pthsged, ptsomeco, ptbaplus, ptpov, pctfb \
+                FROM allegations JOIN acs ON (allegations.tractce10 = acs.tract_1);"
 
     invest = "SELECT * FROM investigator_beat_dum1 NATURAL JOIN investigator_beat_dum2;"
 
@@ -20,17 +25,17 @@ def go():
     invest_df = pd.read_sql(invest, conn)
     age_df = pd.read_sql(age, conn)
     data311_df = pd.read_sql(data311, conn)
-    acs_df = pd.read_sql(acs, conn)
-
+    #Close connection to database after making queries
     conn.commit()
     conn.close()
-
-    # data311_df.rename(columns = {'tract_1' : 'tractce10'}, inplace = True)
-
+    #Merge (join) dataframes on shared keys
     df_final = alleg_df.merge(invest_df, on =['crid', 'officer_id'], how = 'left')\
                 .merge(age_df, on = ['crid', 'officer_id'], how = 'left')
-    df_final = df_final.merge(data311, on = 'crid', how = 'left')\
-                .merge(acs_df, how = 'left', left_on = 'tractce10', right_on = 'tract_1')
-
+    df_final = df_final.join(data311.drop('crid', axis = 1))
+    #Drop sequential index column
+    df_final.drop('index', axis = 1, inplace = True)
 
     return df_final
+
+if __name__ == '__main__':
+    go()
