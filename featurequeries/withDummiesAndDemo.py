4e9e13754142ef066d01e94a3c8ed2c46178d4f5
lmcindewar <lmcindewar@gmail.com>
2016-06-03 07:36:50 -0500

    changes

--- featurequeries/withDummiesAndDemo.py
+++ featurequeries/withDummiesAndDemo.py
@@ -7,17 +7,24 @@ def go():
     conn = psycopg2.connect("dbname = police user = lauren password = llc")
 
     #Queries for features
-    alleg = "SELECT crid, officer_id, tractce10,\
-                (CASE WHEN EXTRACT(dow FROM dateobj) NOT IN (0, 6) THEN 1 ELSE 0 END) AS weekend \
-                FROM allegations \
+    alleg = "SELECT crid, a.officer_id, tractce10, o.race_edit, \
+                (CASE WHEN EXTRACT(dow FROM a.dateobj) NOT IN (0, 6) THEN 1 ELSE 0 END) AS weekend, \
+                (CASE WHEN o.rank IS NOT NULL THEN o.rank ELSE 'UNKNOWN' END) AS rank, \
+                (CASE WHEN investigator_name IN (SELECT concat_ws(', ', officer_last, officer_first) \
+                FROM officers) THEN 1 ELSE 0 END) AS police_investigator \
+                FROM allegations as a LEFT JOIN officers as o \
+                ON (a.officer_id = o.officer_id) \
+                LEFT JOIN investigators AS i ON (a.investigator_id = i.investigator_id) \
                 WHERE tractce10 IS NOT NULL;"
 
     invest1 = "SELECT * FROM investigator_beat_dum1;"
+
     invest2 = "SELECT * FROM investigator_beat_dum2;"
 
     age = "SELECT crid, officer_id, officers_age, (officers_age^2) AS agesqrd FROM ages;"
 
     data311 = "SELECT * FROM time_distance_311;"
+
     datacrime = "SELECT * FROM time_distance_crime;"
 
     priors = "SELECT * FROM prior_complaints;"
@@ -25,6 +32,9 @@ def go():
     acs = "SELECT tract_1, pct017, pct1824, pct2534, pct3544, pct4554, pct5564, pct6500, \
                 ptnla, ptnlb, ptnlwh, ptnloth, ptl, ptlths, pthsged, ptsomeco, ptbaplus, ptpov, pctfb \
                 FROM acs;"
+
+    officer_gender = "SELECT officer_first, gender FROM officers;"
+
 
     alleg_df = pd.read_sql(alleg, conn)
     invest1_df = pd.read_sql(invest1, conn)
@@ -34,8 +44,9 @@ def go():
     datacrime_df = pd.read_sql(datacrime, conn)
     priors_df = pd.read_sql(priors, conn)
     acs_df = pd.read_sql(acs, conn)
-
+    off_gender_df = pd.read_sql(officer_gender, conn)
     #Close connection to database after queries
+
     conn.commit()
     conn.close()
 
@@ -51,11 +62,26 @@ def go():
                 .merge(priors_df, on = ['crid', 'officer_id'], how = 'left')\
                 .merge(acs_df, how = 'left', left_on = 'tractce10', right_on = 'tract_1')
 
-    #Drop sequential index column
-    #df_final.drop('index', axis = 1, inplace = True)
-    df_final.drop(['tract_1', 'tractce10'], inplace = True)
+    #Dummies for race and rank and drop unneeded columns
+    race_dummies = pd.get_dummies(df_final['race'], prefix = 'Race', prefix_sep = ' ', dummy_na = True)
+    rank_dummies = pd.get_dummies(df_final['rank'], prefix = 'Rank', prefix_sep = ' ', dummy_na = True)
+    df_final = df_final.concat([df_final, race_dummies, rank_dummies], axis = 1)
+    df_final.drop(['tract_1', 'tractce10', 'race', 'rank'], inplace = True)
 
     df_final.to_csv("queriedFeatureResults.csv")
+
+def impute_gender(df, name_col, gender_col):
+    '''Fills in missing gender using Genderize.io API.
+    Takes the dataframe, column with first name, and column with gender'''
+    for i, row in df.iterrows():
+        if pd.isnull(df.ix[i , gender_col]):
+            name = df.ix[i , name_col]
+            result = requests.get('https://api.genderize.io/?name=' + name)
+            gender = result.json()['gender'][:1]
+            #Capitalize gender to match the rest of the table
+            df.set_value(index = i, col = gender_col, value = gender.title())
+    return df
+
 
 if __name__ == '__main__':
     go()
